e89be0e0705e5ee90271dead1948d3d514b1a032
mohetasbor-hue <moheta.sbor@gmail.com>
2026-07-28 16:20:47 +0300

    Update review.py

--- review.py
+++ review.py
@@ -7,7 +7,7 @@ def main():
         raise ValueError("GEMINI_API_KEY secret is missing in GitHub settings!")
 
     # Using gemini-2.5-flash
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={api_key}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={api_key}"
     
     payload = {
         "contents": [{
